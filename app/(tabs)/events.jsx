14ef84bea13aa3ce769ff006fd1fc79df469588f
ahammedhridoy <developerhridoy@gmail.com>
2025-02-01 21:53:51 +0600

    fix ui

--- app/(tabs)/events.jsx
+++ app/(tabs)/events.jsx
@@ -83,8 +83,9 @@ const Events = () => {
                   />
                   <Text style={styles.title}>{event?.title}</Text>
                   <Text style={styles.date}>
-                    {dayjs(event?.date).format("ddd, DD")}
-                    {dayjs(event?.secondDate).format(" - DD ")}
+                    {dayjs(event?.date).format("ddd, DD ")}
+                    {event?.secondDate &&
+                      ` - ${dayjs(event?.secondDate).format("DD ")}`}
                     {dayjs(event?.date).format("MMM YYYY")}
                   </Text>
                   <TouchableOpacity

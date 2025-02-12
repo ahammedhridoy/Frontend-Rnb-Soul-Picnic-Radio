e5e2f8207a136e6162040f7988562e7d0d02dec5
ahammedhridoy <developerhridoy@gmail.com>
2025-02-13 00:38:13 +0600

    [Fix] Fixing error

--- app/index.jsx
+++ app/index.jsx
@@ -6,9 +6,37 @@ import {
 } from "react-native";
 import { Redirect } from "expo-router";
 import { useUser } from "@clerk/clerk-expo";
+import axios from "axios";
+import { useEffect, useCallback } from "react";
 
 export default function Index() {
   const { user, isLoaded } = useUser();
+
+  // Function to save user data
+  const saveUserToDb = useCallback(async () => {
+    console.log("User:", user);
+    if (!user?.fullName) return;
+
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/v1/user/register",
+        {
+          name: user?.fullName,
+          email: "user?.emailAddress@gmail.com",
+        }
+      );
+      console.log("User saved:", res?.data);
+    } catch (error) {
+      console.error("Failed to save user to DB:", error);
+    }
+  }, [user]);
+
+  // Run once when the user is loaded
+  useEffect(() => {
+    if (user) {
+      saveUserToDb();
+    }
+  }, [user, saveUserToDb]);
 
   if (!isLoaded)
     return (
